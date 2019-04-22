d0441812cd57b2d42d5dee4723fb366d709d7a8f
liuqing <liuqing@sinochain.com>
2019-04-22 15:17:14 +0800

    class 反编译学习

---
+++ clazz-decompiler/src/main/java/com/lq186/tools/decompiler/reader/attribute/frame/SameFrameExtended.java
@@ -0,0 +1,40 @@
+/*    
+    Copyright ©2019 lq186.com 
+ 
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+ 
+        http://www.apache.org/licenses/LICENSE-2.0
+ 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+/*
+    FileName: SameFrameExtended.java
+    Date: 2019/4/19
+    Author: lq
+*/
+package com.lq186.tools.decompiler.reader.attribute.frame;
+
+import com.lq186.tools.decompiler.reader.basic.U2;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SameFrameExtended extends StackMapFrame {
+
+    private final U2 offsetDeltaU2 = new U2();
+
+    @Override
+    public void read(InputStream inputStream) throws IOException {
+        offsetDeltaU2.read(inputStream);
+    }
+
+    public short getOffsetDelta() {
+        return offsetDeltaU2.getValue();
+    }
+}
